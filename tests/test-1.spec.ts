dd8a4d46fc96f7f284207dc297abefb3576b26e3
Victor Granados Rodriguez <vgranados@namutek.com>
2022-10-15 19:12:43 -0600

    Ejemplo validar pagina kashlatam

---
+++ tests/test-1.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+
+  await page.goto('https://kashlatam.com/');
+
+  await page.locator('.attachment-large').first().click();
+
+  await page.getByRole('heading', { name: 'Selecciona el país del cual nos visitas' }).click();
+
+  await page.locator('section:has-text("Guatemala Costa Rica")').getByRole('link').nth(2).click();
+  await expect(page).toHaveURL('https://kashlatam.com/cr/');
+
+  await page.getByRole('heading', { name: 'Cambiá la forma' }).click();
+
+  await page.getByText('de transferir dinero').click();
+
+  await page.locator('section:has-text(".st0{fill:#FFFFFF;} .st1{fill:#01BBD3;} .st2{fill:#03CEAC;} Cambiá la formade tr")').click();
+
+  await page.locator('section:has-text(".st0{fill:#FFFFFF;} .st1{fill:#01BBD3;} .st2{fill:#03CEAC;} Cambiá la formade tr")').getByRole('link').click();
+
+});
